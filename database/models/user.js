de955c0aecae79e49fcedacf7875bc29a904b66f
whoisPratish <pratish.dhakal2059@gmail.com>
2023-12-04 21:36:50 -0500

    Changes made by Niranjan Adhikari
    Hi, I have made changes for user.js file. And also, I made sure everything is going well. Now, it is my job to make video for the functionality. Thanks!

--- database/models/user.js
+++ database/models/user.js
@@ -1,9 +1,13 @@
 const { mongoose } = require("mongoose");
 
+// Defining mongoose schema for 'User' model with fields for username, password, and email
+
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
 });
+
+// Exporting the 'User' model based on the defined schema
 
 module.exports = mongoose.model("User", userSchema);
